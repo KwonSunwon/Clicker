e94c0af20dbc09cba3d8c033c801a3976bb27848
Eon JangHeon <mpmpm0610@gmail.com>
2025-08-18 21:49:15 +0900

    Chore: 프레임워크 일부 import

---
+++ Assets/0_Scripts/Data/Data.Contents.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data
+{ 
+#region Stat
+	[Serializable]
+	public class PlayerStat
+	{
+		public float WalkSpeed;
+		public float RunSpeed;
+		public float MaxStamina;
+		public float RegenStaminaPerSecond;
+		public float AdditionalStaminaPerCookie;
+		public float NeedJumpStamina;
+		public float NeedRunStaminaPerSec;
+
+		//플레이어 소리범위설정용
+		public float WalkNoiseRange = 30f;
+        public float RunNoiseRange = 50f;
+        public float LandNoiseRange = 100f;
+    }
+
+	[Serializable]
+	public class GameSetting
+	{
+		public float CameraMaxPitch;
+		public float CameraMinPitch;
+		public float InteractionDistance;
+		public float MouseSensitivity;
+		
+	}
+
+	//[Serializable]
+	//public class StatData : ILoader<int, Stat>
+	//{
+	//	public List<Stat> stats = new List<Stat>();
+
+	//	public Dictionary<int, Stat> MakeDict()
+	//	{
+	//		Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
+	//		foreach (Stat stat in stats)
+	//			dict.Add(stat.level, stat);
+	//		return dict;
+	//	}
+	//}
+	#endregion
+
+
+	[Serializable]
+	public class Dialog
+	{
+		public string name;
+		public string text;
+	}
+
+	[Serializable]
+	public class DialogData : ILoader<string, string>
+	{
+		public List<Dialog> dialogs = new List<Dialog>();
+
+		public Dictionary<string, string> MakeDict()
+		{
+			Dictionary<string, string> dict = new Dictionary<string, string>();
+			foreach (Dialog dialog in dialogs)
+				dict.Add(dialog.name, dialog.text);
+			return dict;
+		}
+	}
+
+}
---
+++ Assets/0_Scripts/Managers/Core/DataManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILoader<Key, Value>
+{
+    Dictionary<Key, Value> MakeDict();
+}
+
+public class DataManager
+{
+    //public Dictionary<int, Data.PlayerStat> StatDict { get; private set; } = new Dictionary<int, Data.Stat>();
+
+    public Data.PlayerStat PlayerStat { get; private set; }
+    public Data.GameSetting GameSetting { get; private set; }
+
+	public Dictionary<string, string> DialogDict { get; private set; } = new Dictionary<string, string>();
+
+
+	public void Init()
+    {
+        //StatDict = LoadJson<Data.StatData, int, Data.Stat>("StatData").MakeDict();
+
+		PlayerStat = LoadJson<Data.PlayerStat>("PlayerStat");
+		GameSetting = LoadJson<Data.GameSetting>("GameSetting");
+		DialogDict = LoadJson<Data.DialogData, string, string>("Dialog_ko").MakeDict();
+
+
+	}
+
+	Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
+    {
+		TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Data/{path}");
+        return JsonUtility.FromJson<Loader>(textAsset.text);
+	}
+
+	Loader LoadJson<Loader>(string path)
+	{
+		TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Data/{path}");
+		return JsonUtility.FromJson<Loader>(textAsset.text);
+	}
+}
