23b3c3e5c7bbe403c3f19b2bc5bf547d2583e00e
KwonSunwon <kso00011147@gmail.com>
2025-08-19 22:21:44 +0900

    Test: Steamworks 동작 테스트

--- Assets/0_Scripts/TEST/SteamScript.cs
+++ Assets/0_Scripts/TEST/SteamScript.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using System.Collections;
 using Steamworks;
+using System;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class SteamScript : MonoBehaviour
 {
+    protected Callback<GameOverlayActivated_t> m_GameOverlayActivated;
+
     void Start()
     {
         if (SteamManager.Initialized)
