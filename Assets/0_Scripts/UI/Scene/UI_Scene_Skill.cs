9c8004be84342aa817fc3a64704fbb89dfb1d093
Eon JangHeon <mpmpm0610@gmail.com>
2025-09-12 16:22:41 +0900

    Feat: 설정에 DOTween으로 애니메이션 적용

--- Assets/0_Scripts/UI/Scene/UI_Scene_Skill.cs
+++ Assets/0_Scripts/UI/Scene/UI_Scene_Skill.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -70,13 +71,46 @@ public class UI_Scene_Skill : UI_Scene
 
 	public void ClickedSettingButton(PointerEventData eventData)
 	{
-		var settingPanel = Get<GameObject>((int)GameObjects.UI_Setting_Panel);
-		settingPanel.SetActive(!settingPanel.activeSelf);
+		var panel = Get<GameObject>((int)GameObjects.UI_Setting_Panel);
+
+		bool isActive = panel.activeSelf;
+
+		if (!isActive) // 켤 때
+		{
+			panel.SetActive(true);
+			// DOTween 초기화
+			panel.transform.localScale = Vector3.zero;
+
+			// 1초 동안 0 -> 1 스케일 업
+			panel.transform.DOScale(Vector3.one, 1f)
+				.SetEase(Ease.OutBack); // 부드럽게 튀어나오는 느낌
+		}
+		else // 끌 때
+		{
+			// 꺼질 때 애니메이션도 넣고 싶으면 여기
+			panel.transform.DOScale(Vector3.zero, 0.5f)
+				.SetEase(Ease.InBack)
+				.OnComplete(() => panel.SetActive(false));
+		}
+
+		//UI_Scene_Skill.GrobalClickEvent(eventData);
 
 	}
 
 	public void CloseSettingButton(PointerEventData eventData)
 	{
-		Get<GameObject>((int)GameObjects.UI_Setting_Panel).SetActive(false);
+
+		if (!Get<GameObject>((int)GameObjects.UI_Setting_Panel).activeSelf) return;
+		//Debug.Log(data.pointerClick.name);
+		if (eventData.pointerClick.gameObject != gameObject)
+		{
+			var panel = Get<GameObject>((int)GameObjects.UI_Setting_Panel);
+			panel.transform.DOScale(Vector3.zero, 0.5f)
+				.SetEase(Ease.InBack)
+				.OnComplete(() => panel.SetActive(false));
+			//Get<GameObject>((int)GameObjects.UI_Skill_Explain_Panel).SetActive(false);
+		}
+
+
 	}
 }
